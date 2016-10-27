2384756ca058ef99c98e4b6aeed61a0a1aee729a
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-10-27 17:47:06 +0300

    Create main.cpp

---
+++ examples/main.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <iostream>
+#include <curl/curl.h>
+
+int main()
+{
+	CURL *curl = curl_easy_init();
+	if (curl) {
+		CURLcode res;
+		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+		res = curl_easy_perform(curl);
+		if (res == CURLE_OK) {
+			long response_code;
+			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+			std::cout << "RESPONSE_CODE " << response_code << std::endl;
+		}
+		curl_easy_cleanup(curl);
+	}
+	system("pause");
+    return 0;
+}
